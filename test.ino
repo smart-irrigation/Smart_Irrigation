fef40e5b8040077bf8f9e24e358720505c56f22d
dhrumil1202 <34359170+dhrumil1202@users.noreply.github.com>
2019-10-06 10:24:21 +0530

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,324 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>                 
+#include <WiFiUdp.h>                                  
+#include <FirebaseArduino.h>                                               
+#include <ArduinoJson.h>
+
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+#define firebase_host "temp-nodewise.firebaseio.com"
+#define firebase_auth "WrXvNdIzt9lyGonFsevXW94KpijCfHJZ3pN6Tpws"
+
+
+//variables
+int i = 0;
+int statusCode;
+const char* ssid = "text";
+const char* passphrase = "text";
+String st;
+String content;
+
+bool testWifi(void);
+void launchWeb(void);
+void setupAP(void);
+
+
+WiFiUDP ntpUDP;
+const int moisture_pin =A0;
+
+const long utcOffsetInSeconds = 19800;   // off set of current timezone from utc
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);   // for extraction of current date and time from pool.ntp.org with the off set from timezone
+
+//Establishing Local server at port 80 whenever required
+ESP8266WebServer server(80);
+
+void setup() {
+
+  Serial.begin(9600);
+  delay(2000);                
+  
+  Serial.println("Disconnecting previously connected WiFi");
+  WiFi.disconnect();
+  EEPROM.begin(512); //Initialasing EEPROM
+  delay(10);
+
+  Serial.println("Startup");
+
+  //---------------------------------------- Read eeprom for ssid and pass
+  Serial.println("Reading EEPROM ssid");
+
+  String esid;
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  Serial.println();
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+
+  String epass = "";
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
+  Serial.print("PASS: ");
+  Serial.println(epass);
+
+
+  WiFi.begin(esid.c_str(), epass.c_str());
+  if (testWifi())
+  {
+    Serial.println("Succesfully Connected!!!");
+    timeClient.begin();                                                       // timeclient begin for extracting hrs,minutes,seconds                                   
+    Firebase.begin(firebase_host, firebase_auth);
+    return;
+  }
+  else
+  {
+    Serial.println("Turning the HotSpot On");
+    launchWeb();
+    setupAP();// Setup HotSpot
+  }
+
+  Serial.println();
+  Serial.println("Waiting.");
+  
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    Serial.print(".");
+    delay(100);
+    server.handleClient();
+  }
+  
+                               //connection with firebase is begin here
+}
+void reset_fun()
+{
+  Serial.println("Resetting the controller.....");
+  ESP.restart();
+}
+
+ 
+String formattedDate;                                                     
+String dayStamp;
+int mcu_reset;
+
+void loop() {
+
+ timeClient.update();
+   
+    formattedDate =(String)timeClient.getFormattedDate();
+    int splitT = formattedDate.indexOf("T");
+    dayStamp = formattedDate.substring(0, splitT);
+    Serial.println(dayStamp);
+    
+    String hrs=(String)timeClient.getHours();
+    String min1=(String)timeClient.getMinutes();
+    String time1=(String)timeClient.getHours()+":"+(String)timeClient.getMinutes()+":"+(String)timeClient.getSeconds();
+    Serial.println(time1);
+    mcu_reset=Firebase.getInt("RESET_NODES/RESET_NODE1");
+    Serial.println(mcu_reset);
+    if(mcu_reset==1)
+    {
+      Firebase.setInt("RESET_NODES/RESET_NODE1",0);
+      reset_fun();
+    }
+    //block of moisture starts here
+     
+    int moisture_value=analogRead(moisture_pin);         // moisture value extract 
+    
+    Serial.println(moisture_value);
+
+    //block of moisture ends here
+
+     //firebase upload of the soil moisture data starts here
+    Firebase.setInt("/node_data/node2/"+dayStamp+"/Moisture/"+time1, moisture_value);
+     if(Firebase.failed())
+    {
+      Serial.println("Failed!!!");
+    }
+    else{
+      Serial.println("success");
+      }
+    Firebase.setInt("/node_data/allnodedata/node2data",moisture_value);
+    if(Firebase.failed())
+    {
+      Serial.println("Failed!!!");
+    }
+    else{
+      Serial.println("success");
+      }
+      
+    if(moisture_value>500){
+      int loopval=9;
+      int temp_value;
+      while(loopval>0)
+      {
+        temp_value=analogRead(moisture_pin);
+        Firebase.setInt("/node_data/node2/loop_value/value",temp_value);
+        Firebase.setInt("/node_data/allnodedata/node2data",temp_value);
+        Serial.print(temp_value);
+        loopval--;
+        delay(4000);
+      }
+     loopval=9; 
+    }
+    else{
+    delay(30000);
+    }
+    //firebase upload of the soil moisture data ends here
+
+}
+//----------------------------------------------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change 
+bool testWifi(void)
+{
+  int c = 0;
+  Serial.println("Waiting for Wifi to connect");
+  while ( c < 30 ) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("enter wifi credentials", "");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+
+void createWebServer()
+{
+ {
+    server.on("/", []() {
+
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+         ESP.restart();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+
+    });
+  } 
+}
