0671c46beb1b423b330bfd01b0551fc0089fb71b
Meet Gandhi <meet.gandhi1415@gmail.com>
2019-10-06 10:40:41 +0530

    Update test.ino

--- test.ino
+++ test.ino
@@ -195,7 +195,9 @@ void launchWeb()
 {
   Serial.println("");
   if (WiFi.status() == WL_CONNECTED)
+  {
     Serial.println("WiFi connected");
+  }
   Serial.print("Local IP: ");
   Serial.println(WiFi.localIP());
   Serial.print("SoftAP IP: ");
